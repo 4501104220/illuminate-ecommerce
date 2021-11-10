15abc1766eddbb8ba40836be6a7fe734ef044929
4501104220 <thangtrantt@hotmail.com>
2021-11-10 17:47:00 +0700

    update: change id tawk.to

--- frontend/src/modules/Shop/Components/ShopLayout/ShopLayout.js
+++ frontend/src/modules/Shop/Components/ShopLayout/ShopLayout.js
@@ -37,7 +37,7 @@ export default function ShopLayout({children}) {
             var s1 = document.createElement("script"),
                 s0 = document.getElementsByTagName("script")[0];
             s1.async = true;
-            s1.src = "https://embed.tawk.to/5fade92f7279c47e5dcf68f8/default";
+            s1.src = "https://embed.tawk.to/6187486f6885f60a50baa815/1fjs75tn6";
             s1.charset = "UTF-8";
             s1.setAttribute("crossorigin", "*");
             s0.parentNode.insertBefore(s1, s0);
